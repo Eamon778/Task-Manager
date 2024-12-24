a2ca5428002fbc41b0bfa5eb24bd6dff92b992b8
Eamon Ali <eamonmondol525@gmail.com>
2024-12-24 13:24:46 +0600

    adding mongodb online

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use('/api', router);
 
 const start = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/book-store');
+    await mongoose.connect(process.env.MONGO_URI);
     app.listen(process.env.PORT, () => {
         console.log(`Server is running on port: ${process.env.PORT}`);
     });
